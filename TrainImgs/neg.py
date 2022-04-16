d5110d4fab725021876aaf9b4c35ed49ea3404d4
Notstevee <howaihappy2011@gmail.com>
2022-04-16 13:06:54 +0800

    from server

--- TrainImgs/neg.py
+++ TrainImgs/neg.py
@@ -9,5 +9,7 @@ def generate_negative_description_file():
 
 generate_negative_description_file()
 
-#C:\Users\howai\Downloads\opencv\build\x64\vc15\bin\opencv_createsamples.exe -info C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\pos.txt -w 20 -h 20 -num 5000 -vec pos.vec
-#C:\Users\howai\Downloads\opencv\build\x64\vc15\bin\opencv_traincascade.exe -data C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\cascade\  -vec C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\pos.vec -bg C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\neg.txt -numPos 200 -numNeg 100 -numStages 10 -w 20 -h 20
+#C:\Users\howai\Downloads\opencv\build\x64\vc15\bin\opencv_createsamples.exe -info C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\Import_code_and_image_store\pos.txt -w 20 -h 20 -num 5000 -vec pos.vec
+#C:\Users\howai\Downloads\opencv\build\x64\vc15\bin\opencv_traincascade.exe -data C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\cascade\  -vec C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\pos.vec -bg C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\TrainImgs\neg.txt -numPos 200 -numNeg 100 -numStages 20 -w 20 -h 20
+
+#C:\Users\howai\Downloads\opencv\build\x64\vc15\bin\opencv_annotation.exe --annotations=C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\Import_code_and_image_store\[AID01111]\pos.txt --images=C:\Stuff\HKU\Git\CarsML\Traffic-flow-prediction\Import_code_and_image_store\[AID01111]
