ff89456d83ce1cfbf29f0832af7c9c5ed5d9b71f
Notstevee <howaihappy2011@gmail.com>
2022-04-16 21:12:42 +0800

    recogs

--- ImageRecog/ImgRecog.py
+++ ImageRecog/ImgRecog.py
@@ -1,27 +1,55 @@
 import cv2
 import numpy as np
+import os
+import csv
 
+files=["[AID01111]","[AID01213]","[AID03106]","[AID03211]","[TDSIEC10001]"]
+def ImgRecog(files):
 
-img_fl=r"TrainImgs\Previous train image\aa688a20-15a7-4677-b6af-e99fbf8acef0.jpg"
-class_file=r"TrainImgs\cascade\cascade.xml"
+    os.chdir(r"Import_code_and_image_store")
 
-img=cv2.imread(img_fl)
+    
 
-b_n_w=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    p=1
+    for i in files:
+        fd=open(i+'.csv','a')
+        for filename in os.listdir(i):
+            img_fl = os.path.join(i,filename)
+            
+            #img_fl="Import_code_and_image_store\\"+i+"\\"+filename
+            
+            
+            
+            class_file="cascade.xml"
+            
+            
 
-car_track=cv2.CascadeClassifier(class_file)
+            img=cv2.imread(img_fl)
+            ugly=cv2.imread("Index0_Hong_Chong_Road_near_The_Hong_Kong_Polytechnic_University_-_Northbound_(1)_[AID01111]_2022_04_10_21_29_18.jpg")
+            if p==1:
+                last=ugly
+                p=-1
+            try:
+                if not(np.bitwise_xor(ugly,img).any()) or not(np.bitwise_xor(img,last).any()) or type(img)=="NoneType":
+                    print("identical/bad img:",filename)
+                    #continue
+                else:
+                    #try:
+                    b_n_w=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-cars=car_track.detectMultiScale(b_n_w)
+                    car_track=cv2.CascadeClassifier(class_file)
 
-for (x,y,w,h) in cars:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+                    cars=car_track.detectMultiScale(b_n_w)
+                    fd.write("{},{}\n".format(filename[-23:-4],len(cars)))
+                    #except:
+                        #print(filename)
 
+                
+            except:
+                print(filename)
+            last=img
+        fd.close()    
 
-cv2.imshow("Name", img)
-
-cv2.waitKey()
-
-print("yes")
-
+ImgRecog(files)  
 
 #https://www.analyticsvidhya.com/blog/2020/04/vehicle-detection-opencv-python/
---
+++ Import_code_and_image_store/NeuralNetwork.py
@@ -0,0 +1,6 @@
+import os
+import datetime
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
